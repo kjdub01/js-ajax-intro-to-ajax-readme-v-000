a1a61058fb63e87c4e77c7899e6d4f4394d12fe3
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-23 22:07:25 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -14,6 +14,21 @@ function showRepositories() {
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
  
 function getRepositories() {
   const req = new XMLHttpRequest();
